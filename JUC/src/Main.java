13ae20186a8c65f166f011b7edd372a8c4121dd9
Yicheng Lu <442570684@qq.com>
2022-05-14 00:07:09 +0800

    init JUC

--- JUC/src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-
-        System.out.println("Hello world!");
-        String passwd = "aW9\EAT$HpF83(d74?iL5=45%a]Z~Y5*";
-    }
-
-}
